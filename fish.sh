6cd50f13a79526d70a66fb56e8ac10ce1de747e2
Starlight Romero <starlightromero@protonmail.com>
2021-02-13 08:46:51 -0800

    [update] added keys and repositories

--- fish.sh
+++ fish.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+# upgrade full system
+apt-get -y full-upgrade
+# install packages to allow apt to use a repository over HTTPS
+apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    git-all
+# add keys
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# add repositories
+add-apt-repository -y ppa:deadsnakes/ppa
+add-apt-repository -y ppa:libreoffice/ppa
+add-apt-repository -y ppa:pinta-maintainers/pinta-stable
+add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
+add-apt-repository -y ppa:phoerious/keepassxc
+add-apt-repository -y https://cli.github.com/packages
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+# Go
+tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 # Fish
 add-apt-repository -y ppa:fish-shell/release-3
 apt-get -y update && apt-get -y upgrade
