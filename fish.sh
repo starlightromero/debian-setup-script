80982a1241c1aecccbd007417831b8bd07eaa7d9
Starlight Romero <starlightromero@protonmail.com>
2021-02-13 08:40:01 -0800

    [update[ added fish repository

--- fish.sh
+++ fish.sh
@@ -1,4 +1,6 @@
 # Fish
+add-apt-repository -y ppa:fish-shell/release-3
+apt-get -y update && apt-get -y upgrade
 apt-get -y install fish
 mkdir /home/starlight/.config/fish
 mv ./config.fish /home/starlight/.config/fish/
