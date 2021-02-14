7af13ea4e330da19002b9cff5cd9398eb7125b67
Starlight Romero <starlightromero@protonmail.com>
2021-02-14 14:14:03 -0800

    [update] added sudo tar

--- fish.sh
+++ fish.sh
@@ -26,7 +26,7 @@ sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/u
    stable"
 # Go
 wget -c https://golang.org/dl/go1.15.8.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 # Fish
 sudo add-apt-repository -y ppa:fish-shell/release-3
