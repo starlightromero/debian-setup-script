7bf9c36f589a800e97aec469692bc1df363b4e1c
Starlight Romero <starlightromero@protonmail.com>
2021-02-14 13:41:42 -0800

    [update] bug fixes

--- setup.sh
+++ setup.sh
@@ -40,9 +40,10 @@ apt-get -y install --no-install-recommends yarn \
     libffi-dev \
     liblzma-dev
 # NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | fish
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 omf install https://github.com/fabioantunes/fish-nvm
 omf install https://github.com/edc/bass
+mkdir /home/starlight/.config/fish/functions
 echo "function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end" >> /home/starlight/.config/fish/functions/nvm.fish
