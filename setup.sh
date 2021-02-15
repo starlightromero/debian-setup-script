be5396428fed1e158dafdd493f1eff968ea5b487
Starlight Romero <starlightromero@protonmail.com>
2021-02-14 17:09:21 -0800

    [update] added brave key and repo

--- setup.sh
+++ setup.sh
@@ -27,6 +27,8 @@ chmod +x ./Session
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 wget https://www.privateinternetaccess.com/installer/pia-nm.sh
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 # WhiteSur
 git clone https://github.com/vinceliuice/WhiteSur-kde.git
 cd WhiteSur-kde
